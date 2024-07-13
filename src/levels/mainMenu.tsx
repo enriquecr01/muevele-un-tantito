661e830b02a51c124204be4d97b1df771628b843
Enrique Chavez R. <30837445+enriquecr01@users.noreply.github.com>
2024-07-12 22:48:03 -0700

    feat(MainMenu): :sparkles: wip of main menu

--- src/levels/mainMenu.tsx
+++ src/levels/mainMenu.tsx
@@ -3,16 +3,17 @@ import "./../App.css";
 
 const MainMenu = () => {
   return (
-    <>
-      <section>
-        <h1>MUEVELE UN TANTITO</h1>
-        <button className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4">
-          <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]">
+    <div className="w-full">
+      <section className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-6xl font-extrabold">MUEVELE UN TANTITO</h1>
+
+        <button className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4 w-1/2 focus:border-neutral-950/20 hover:ring-orange-600 hover:border-neutral-950/20 active:border-neutral-950/20">
+          <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)] text-2xl">
             Button
           </span>
         </button>
       </section>
-    </>
+    </div>
   );
 };
 
