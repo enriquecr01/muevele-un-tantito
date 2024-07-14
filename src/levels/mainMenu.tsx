49d7e3853d77cd38224e30d453b8cf6af36f681a
Enrique Chavez R. <30837445+enriquecr01@users.noreply.github.com>
2024-07-13 17:19:45 -0700

    style(MainMenu): :lipstick: added possible fonts to utilize on the UI

--- src/levels/mainMenu.tsx
+++ src/levels/mainMenu.tsx
@@ -3,17 +3,27 @@ import "./../App.css";
 
 const MainMenu = () => {
   return (
-    <div className="w-full">
-      <section className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-6xl font-extrabold">MUEVELE UN TANTITO</h1>
+    <>
+      <div className="w-full">
+        <section className="flex flex-col items-center justify-center gap-12 h-screen">
+          <h1 className="font-avocado text-6xl font-extrabold">
+            ¡MUÉVELE UN TANTITO!
+          </h1>
+          <h1 className="font-comic text-6xl font-extrabold">
+            ¡MUÉVELE UN TANTITO!
+          </h1>
+          <h1 className="font-laff text-6xl font-extrabold">
+            ¡MUÉVELE UN TANTITO!
+          </h1>
 
-        <button className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4 w-1/2 focus:border-neutral-950/20 hover:ring-orange-600 hover:border-neutral-950/20 active:border-neutral-950/20">
-          <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)] text-2xl">
-            Button
-          </span>
-        </button>
-      </section>
-    </div>
+          <button className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4 w-1/2 focus:border-neutral-950/20 hover:ring-orange-600 hover:border-neutral-950/20 active:border-neutral-950/20">
+            <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)] text-2xl">
+              Button
+            </span>
+          </button>
+        </section>
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      avocado: ["AvocadoTaco"],
+      comic: ["ComicZine"],
+      laff: ["LaffRiot"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
