b986698c1cb031e8b9e95b25e641c2391b73f839
Enrique Chavez R. <30837445+enriquecr01@users.noreply.github.com>
2024-07-16 21:33:35 -0700

    feat(MainMenu): :sparkles: added option to exit and Font Title selected

--- src/levels/mainMenu.tsx
+++ src/levels/mainMenu.tsx
@@ -10,21 +10,26 @@ const MainMenu = () => {
       </Helmet>
       <div className="w-full">
         <section className="flex flex-col items-center justify-center gap-12 h-screen">
-          <h1 className="font-avocado text-6xl font-extrabold">
-            ¡MUÉVELE UN TANTITO!
-          </h1>
           <h1 className="font-comic text-6xl font-extrabold">
             ¡MUÉVELE UN TANTITO!
           </h1>
-          <h1 className="font-laff text-6xl font-extrabold">
-            ¡MUÉVELE UN TANTITO!
-          </h1>
-
-          <button className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4 w-1/2 focus:border-neutral-950/20 hover:ring-orange-600 hover:border-neutral-950/20 active:border-neutral-950/20">
-            <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)] text-2xl">
-              Button
-            </span>
-          </button>
+          <div className="flex flex-col items-center w-full gap-1.5">
+            <button className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4 w-1/2 focus:border-neutral-950/20 hover:ring-orange-600 hover:border-neutral-950/20 active:border-neutral-950/20">
+              <span className="drop-shadow-[0_2px_2px_rgba(255,255,255,0.9)] text-2xl">
+                Jugar
+              </span>
+            </button>
+            <button
+              className="bg-transparent hover:bg-neutral-950/20 text-white py-2 px-4 w-1/2 focus:border-neutral-950/20 hover:ring-orange-600 hover:border-neutral-950/20 active:border-neutral-950/20"
+              onClick={() => {
+                window.close();
+              }}
+            >
+              <span className="drop-shadow-[0_2px_2px_rgba(255,255,255,0.9)] text-2xl">
+                Salir
+              </span>
+            </button>
+          </div>
         </section>
       </div>
     </>
